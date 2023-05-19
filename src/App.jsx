e2dec87020bdb2455ceed446a80f9d9cc561bcf1
javier aldana <jdad1701@gmail.com>
2023-05-19 21:39:08 +0200

    imagenes rotas corregidas

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
       <div id="container" className="mx-5">
         <Jumbotron />
       <div id="cards" className="d-flex justify-content-evenly">
-        <Card className="col" title="Venezuela" desc="Viva Venezuela en esa mierda caballero" srcImage="./img/WhatsApp Image 2023-05-18 at 11.43.35 AM.jpeg" />
-        <Card className="col" title="Pedri" desc="Pedri con bigote" srcImage="img\WhatsApp Image 2023-05-18 at 11.43.23 AM.jpeg"/>
-        <Card className="col" title="Mario" desc="Mario loves you very much" srcImage="img\WhatsApp Image 2023-05-18 at 11.46.43 AM.jpeg"/>
-        <Card className="col" title="😁" desc="😁" srcImage="img\WhatsApp Image 2023-05-18 at 12.16.38 PM.jpeg"/>
+        <Card className="col" title="Venezuela" desc="Viva Venezuela en esa mierda caballero" srcImage="https://i.ibb.co/N9s4mRb/Whats-App-Image-2023-05-18-at-11-43-35-AM.jpg" alt="venezuela el mejor pais del mundo"/> 
+        <Card className="col" title="Pedri" desc="Pedri con bigote" srcImage="https://i.ibb.co/dWJnKGH/Whats-App-Image-2023-05-18-at-11-43-23-AM.jpg" alt="literalmente pedri con bigote, y calvo"/>
+        <Card className="col" title="Mario" desc="Mario loves you very much" srcImage="https://i.ibb.co/rkrV2LP/Whats-App-Image-2023-05-18-at-11-46-43-AM.jpg" alt="Mario loves you very much"/>
+        <Card className="col" title="😁" desc="😁" srcImage="https://i.ibb.co/rkrV2LP/Whats-App-Image-2023-05-18-at-11-46-43-AM.jpg" alt="😁"/>
         </div>
       </div>
       <Footer />
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+ /**<img src="" alt="Whats-App-Image-2023-05-18-at-11-46-43-AM" border="0">*/
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 function Card(props) {
     return <div id="card" className="card" style={{width: "18rem"}}>
-    <img src= {props.srcImage} width={"100%"} height={325}></img>
+    <img src= {props.srcImage} width={"100%"} height={325} alt={props.alt}></img>
     <h2>{props.title}</h2>
     <p>{props.desc}</p>
     <button className="btn btn-primary">Find Out More!</button>
